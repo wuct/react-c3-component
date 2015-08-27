a728441047697788e43798d7352b1bdd975b7460
wuct <wu.chingting@gmail.com>
2015-08-28 00:55:51 +0800

    add C3Chart

---
+++ src/C3Chart.js
@@ -0,0 +1,27 @@
+import { default as React, findDOMNode } from 'react';
+import pureRender from 'pure-render-decorator';
+import c3 from 'c3';
+import 'c3/c3.css';
+
+@pureRender
+export default class C3Chart extends React.Component {
+  componentDidMount() {
+    this._renderChart();
+  }
+
+  componentDidUpdate() {
+    this._renderChart();
+  }
+
+  render() {
+    return <div style={this.props.style} />;
+  }
+
+  _renderChart() {
+    const { style, ...otherProps } = this.props;
+    c3.generate({
+      bindto: findDOMNode(this),
+      ...otherProps
+    });
+  }
+}
