862ec36b85b04445f013bf8580deae8319b019ae
CT Wu <wu.chingting@gmail.com>
2017-05-27 17:41:27 +0800

    test(Chart): move to jest

--- src/Chart.test.js
+++ src/Chart.test.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { createRenderer } from 'react-addons-test-utils'
-import { expect } from 'chai'
+import { mount } from 'enzyme'
 import Chart from './Chart'
 
-describe('Chart', () => {
-  it('renders a div', () => {
-    const renderer = createRenderer()
-    renderer.render(<Chart />)
-    expect(renderer.getRenderOutput().type).to.equal('div')
-  })
+jest.mock('c3', () => {
+  const mockGenerate = jest.fn()
+
+  return {
+    generate: mockGenerate
+  }
+})
+
+test('call c3.generate', () => {
+  const chart = mount(<Chart config={{ foo: 1 }} />)
+
+  chart.setProps({ config: { foo: 2 }})
+
+  const mockGenerate = require('c3').generate
+  expect(mockGenerate.mock.calls).toMatchSnapshot()
 })
