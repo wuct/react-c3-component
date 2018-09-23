560366fcdad4a73d24cbc64ac689d14d1e098bef
CT Wu <wu.chingting@gmail.com>
2018-09-23 17:42:48 +0800

    Remove findDOMNode from peerDependencies

--- src/Chart.js
+++ src/Chart.js
@@ -1,35 +1,33 @@
-import React from 'react'
-import { findDOMNode } from 'react-dom'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Chart extends React.Component {
   static propTypes = {
     config: PropTypes.object,
-  }
+  };
 
   componentDidMount() {
-    this.c3 = require('c3')
-    this.generateChart()
+    this.c3 = require('c3');
+    this.generateChart();
   }
 
   componentDidUpdate() {
-    this.generateChart()
+    this.generateChart();
   }
 
   componentWillUnmount() {
-    this.chart = this.chart.destroy()
+    this.chart = this.chart.destroy();
   }
 
   generateChart() {
     this.chart = this.c3.generate({
-      bindto: findDOMNode(this),
+      bindto: this.node,
       ...this.props.config,
-    })
+    });
   }
 
   render() {
-    const { config, ...otherProps } = this.props
-    return <div {...otherProps} />
+    const { config, ...otherProps } = this.props;
+    return <div ref={node => (this.node = node)} {...otherProps} />;
   }
-
 }
